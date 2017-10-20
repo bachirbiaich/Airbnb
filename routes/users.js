a2971da0178f2118278a1373ad39cc818200690c
bachirbiaich <bachir.biaich@gmail.com>
2017-10-20 14:58:55 +0200

    Ajout schéma User

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,19 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
+//User schema
+var Schema = mongoose.Schema;
+
+var User = new Schema({
+  username : { type: String, required: true, index: { unique: true } },
+  pass : { type: String, required: true },
+  firstname : { type: String, required: true },
+  lastname : { type: String, required: true },
+  mail : { type: mongoose.SchemaTypes.Email, required: true },
+  phone : { type: String, required: true, validate: { validator: function(v) {return /(\+\d{1-3}|00\d{1-3}|0)\d{9}/.test(v);} } },
+  last_connexion : { type: Date, required: false },
+});
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
