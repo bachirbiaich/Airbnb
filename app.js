9ccd900482d187c5feb2a6afc8ddbee119dcfe0e
bachirbiaich <bachir.biaich@gmail.com>
2017-11-12 17:30:45 +0100

    init booking and apartments

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ mongoose.connect('mongodb://bnbbachir:nmrwDPUnE9N1AkRY@airbnb-shard-00-00-4eqv3.
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var apartments = requite('./routes/apartments');
 
 var app = express();
 
@@ -33,6 +34,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/apartments', apartments);
+app.use('/booking', booking);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
